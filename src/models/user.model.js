2e06cb640dc0fbfebeabae44d3ef2da7311e4a80
DevanshParashar165 <parashardevansh165@gmail.com>
2025-12-15 01:01:44 +0530

    connected database , created user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    id : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    username : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 6
+    },
+    email : {
+        type : String,
+        unique : true,
+        required : true
+    }
+},{timestamps : true})
+
+const User = mongoose.model("User",UserSchema)
+
+export default User;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from "express"
+import "dotenv/config";
+import { app } from "./app.js";
+import connectDB from "./config/db.js";
 
-const app = express();
 
-const port = 5000
+const port = process.env.PORT || 5000
 
-app.listen(port,()=>{
-    console.log(`App is listening on port : ${port}`)
+connectDB()
+.then(()=> {
+    app.listen(port,()=>{
+        console.log(`App is listening on port : ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
