b0c8034ae2d2282b8855a7bd6ec70675e0d9eee8
DevanshParashar165 <parashardevansh165@gmail.com>
2025-12-20 02:59:06 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ import userRouter from "./routes/auth.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 import { errorHandler } from "./middlewares/error.middleware.js"
+import { setupSwagger } from "./config/swagger.js";
 
 const app = express()
 app.use(cors({
     origin : process.env.CORS_ORIGIN
 }))
+
+
+setupSwagger(app);
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
